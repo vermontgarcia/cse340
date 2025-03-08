0b363ffccb894c5b089b67821245df20ac424cb5
Vermont Garcia <vermontgarcia@gmail.com>
2025-03-08 16:33:52 -0700

    Server update

--- server.js
+++ server.js
@@ -5,9 +5,9 @@
 /* ***********************
  * Require Statements
  *************************/
+require("dotenv").config();
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
-const env = require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
 
