8b8a09c815b6694af4840a4c9d087b3e7c23dc55
Vermont Garcia <vermontgarcia@gmail.com>
2025-03-18 19:26:50 -0600

    Get inventory by classification id model

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -4,6 +4,19 @@ const getClassifications = async () => {
   return await pool.query('SELECT * FROM classification ORDER BY clas_name');
 };
 
+const getInventoryByClassificationId = async (clasId) => {
+  try {
+    const { rows } = await pool.query(
+      `SELECT * FROM inventory AS i JOIN classification AS c ON i.clas_id = c.clas_id WHERE i.clas_id = $1`,
+      [clasId]
+    );
+    return rows;
+  } catch (error) {
+    console.log('getInventoryByClassificationId error', error);
+  }
+};
+
 module.exports = {
   getClassifications,
+  getInventoryByClassificationId,
 };
