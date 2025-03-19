ea02a24469fdcc661b1bef0985a83ea23206233a
Vermont Garcia <vermontgarcia@gmail.com>
2025-03-18 20:20:14 -0600

    Basic error handling

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 const static = require('./routes/static');
 const { buildHome } = require('./controllers/baseController');
 const inventoryRouter = require('./routes/inventoryRoute');
+const { getNav } = require('./utilities');
 
 /* ***********************
  * View Engine and Templates
@@ -29,6 +30,28 @@ app.use(static);
 app.get('/', buildHome);
 // Inventory Routes
 app.use('/inv', inventoryRouter);
+
+/* ***********************
+ * Route Not Found
+ * Must be keep after all other routes
+ *************************/
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, it seems that page doesn't exists!" });
+});
+
+/* ***********************
+ * Express Error Handler
+ * Must be keep after all other middleware
+ *************************/
+app.use(async (err, req, res, next) => {
+  nav = await getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  res.render('errors/error', {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav,
+  });
+});
 
 /* ***********************
  * Local Server Information
